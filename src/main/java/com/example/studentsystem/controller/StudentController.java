4a5babd1b98064d3061d098edbebf0ffc1cd22ec
Basic Lady Programmer <sutharsirajan@gmail.com>
2023-03-01 22:47:55 +0530

    First Commit

---
+++ src/main/java/com/example/studentsystem/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.studentsystem.controller;
+
+import com.example.studentsystem.model.Student;
+import com.example.studentsystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+}
